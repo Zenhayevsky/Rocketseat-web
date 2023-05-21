1493c530be533cd7c115e67d4d970009d8ced4b9
Zenhayevsky <74720973+Zenhayevsky@users.noreply.github.com>
2023-05-21 02:56:07 -0300

    tailwind inicio tela
    - cores padr'ap tailwind.
    - fontes  do projeto base
    - favicon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,22 @@ import { Button } from './components/Button';
 
 export default function Home() {
   return (
-    <div className='h-screen bg-zinc-950 p-6 text-zinc-50'>
-      <h1 className='text-4xl font-bold'> Zenitha c.s.a. </h1>
+  <main className='grid grid-cols-2 min-h-screen'>
+
+    {/* left */}
+    <div className='flex flex-col items-start justify-between px-28 overflow-hidden py-16 relative'>
+      <div className='absolute right-0 top-1/2 h-[288px] w-[526px] rounded-full -translate-y-1/2 translate-x-1/2  bg-purple-700 opacity-50'/>
     </div>
-  )
+
+    {/* right */}
+    <div className='flex flex-col p-16'>
+      <div className='flex flex-1 items-center justify-center'>
+        <p className='text-center leading-relaxed w-[360px]'>
+          Você ainda não registrou nenhuma lembrança, comece a{' '}
+          <a href=''className='underline hover:text-gray-50'>criar agora!</a>
+        </p>
+      </div>
+    </div>
+
+  </main>)
 }
